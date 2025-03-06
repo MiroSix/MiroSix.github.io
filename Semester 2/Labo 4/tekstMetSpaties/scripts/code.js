ef8581930da1c0115f1f8e7798def9abb292093b
MiroSix <miro.six@student.vives.be>
2025-03-06 12:25:48 +0100

    labo 4

---
+++ Semester 2/Labo 4/tekstMetSpaties/scripts/code.js
@@ -0,0 +1,20 @@
+const setup = () => {
+    document.getElementById("send").addEventListener("click", () => {
+        let inputText = document.getElementById("txt").value;
+        let spacedText = inputText.split('').join(' ');
+        console.log(spacedText);
+    });
+
+    document.getElementById("sendAn").addEventListener("click", () => {
+        let tekst = document.getElementById("txtAn").value;
+        let zoekSequentie = "an";
+        let teller = 0;
+        let index = tekst.indexOf(zoekSequentie);
+        while (index !== -1) {
+            teller++;
+            index = tekst.indexOf(zoekSequentie, index + 1);
+        }
+        console.log(`De sequentie "${zoekSequentie}" komt ${teller} keer voor.`);
+    });
+}
+window.addEventListener("load", setup);
